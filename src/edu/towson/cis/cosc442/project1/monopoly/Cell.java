a4c63d7e9c9351c010a9a242e0de716401f24233
Zaynab Tabassi <zaynabtabassi@Zaynabs-MacBook-Air.local>
2026-09-24 16:55:22 -0400

    Task 10 -- Extractred and refactored calculateMonopoliesR ent() from getRent() reducing the CCM of getRent() from 4 to 2

--- src/edu/towson/cis/cosc442/project1/monopoly/Cell.java
+++ src/edu/towson/cis/cosc442/project1/monopoly/Cell.java
@@ -1,40 +1,80 @@
 package edu.towson.cis.cosc442.project1.monopoly;
 
+/**
+ * This abstract class represents a cell on the Monopoly board. 
+ * Each cell has a name, an owner (if applicable), and an availability status.
+ * Subclasses of Cell will implement specific behaviors for different types of cells.
+ */
 public abstract class Cell {
 	private boolean available = true;
 	private String name;
 	protected Player theOwner;
 
+	/**
+	 * Returns the name of this cell.
+	 * @return The name of the cell.
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Returns the player who owns this cell.
+	 * @return The owner of the cell, or null if none.
+	 */
 	public Player getTheOwner() {
 		return theOwner;
 	}
 	
+	/**
+	 * Returns the price of this cell, defaulting to 0.
+	 * @return The price of the cell.
+	 */
 	public int getPrice() {
 		return 0;
 	}
 
+	/**
+	 * Indicates whether this cell is currently available.
+	 * @return True if the cell is available, false otherwise.
+	 */
 	public boolean isAvailable() {
 		return available;
 	}
 	
+	/**
+	 * Performs the action triggered when a player lands on this cell.
+	 */
 	public abstract void playAction();
 
+	/**
+	 * Sets the availability status of this cell.
+	 * @param available The new availability state to set.
+	 */
 	public void setAvailable(boolean available) {
 		this.available = available;
 	}
 	
+	/**
+	 * Sets the name of this cell.
+	 * @param name The new name to assign to the cell.
+	 */
 	void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * Assigns the owner of this cell.
+	 * @param owner The player to set as the owner of the cell.
+	 */
 	public void setTheOwner(Player owner) {
 		this.theOwner = owner;
 	}
     
+    /**
+     * Returns a string representation of this cell.
+     * @return The name of the cell as its string representation.
+     */
     public String toString() {
         return name;
     }
--- src/edu/towson/cis/cosc442/project1/monopoly/PropertyCell.java
+++ src/edu/towson/cis/cosc442/project1/monopoly/PropertyCell.java
@@ -1,5 +1,9 @@
 package edu.towson.cis.cosc442.project1.monopoly;
 
+/**
+ * Represents a purhcasable property cell on the Monopoly board. 
+ * This class extends the abstract Cell class and adds specific attributes and behaviors related to properties, such as color group, house price, number of houses, rent, and selling price.
+ */
 public class PropertyCell extends Cell {
 	private String colorGroup;
 	private int housePrice;
@@ -7,36 +11,70 @@ public class PropertyCell extends Cell {
 	private int rent;
 	private int sellPrice;
 
+	/**
+	 * Returns the color group of this property cell.
+	 * @return The color group of the property.
+	 */
 	public String getColorGroup() {
 		return colorGroup;
 	}
 
+	/**
+	 * Returns the price of a house on this property.
+	 * @return The price for building a house on this property.
+	 */
 	public int getHousePrice() {
 		return housePrice;
 	}
 
+	/**
+	 * Returns the number of houses currently built on this property.
+	 * @return The number of houses on the property.
+	 */
 	public int getNumHouses() {
 		return numHouses;
 	}
     
+    /**
+     * Returns the selling price of this property.
+     * @return The selling price of the property.
+     */
     public int getPrice() {
 		return sellPrice;
 	}
 
+	/**
+	 * Calculates and returns the rent to be paid for landing on this property, considering monopolies and houses.
+	 * @return The rent amount to charge the player landing on this property.
+	 */
 	public int getRent() {
 		int rentToCharge = rent;
 		String [] monopolies = theOwner.getMonopolies();
-		for(int i = 0; i < monopolies.length; i++) {
-			if(monopolies[i].equals(colorGroup)) {
-				rentToCharge = rent * 2;
-			}
-		}
+		rentToCharge = caclulateMonopoliesRent(rentToCharge, monopolies);
 		if(numHouses > 0) {
 			rentToCharge = rent * (numHouses + 1);
 		}
 		return rentToCharge;
 	}
 
+	/**
+	 * Calculates the rent to charge based on whether the owner has a monopoly on the color group.
+	 * @param rentToCharge The initial rent amount before monopoly adjustment.
+	 * @param monopolies An array of color groups on which the owner has a monopoly.
+	 * @return The adjusted rent amount after monopoly consideration.
+	 */
+	private int caclulateMonopoliesRent(int rentToCharge, String[] monopolies) {
+		for(int i = 0; i < monopolies.length; i++) {
+			if(monopolies[i].equals(colorGroup)) {
+				rentToCharge = rent * 2;
+			}
+		}
+		return rentToCharge;
+	}
+
+	/**
+	 * Executes the action for when a player lands on this property, including paying rent if applicable.
+	 */
 	public void playAction() {
 		Player currentPlayer = null;
 		if(!isAvailable()) {
@@ -47,22 +85,42 @@ public class PropertyCell extends Cell {
 		}
 	}
 
+	/**
+	 * Sets the color group for this property cell.
+	 * @param colorGroup The color group to assign to the property.
+	 */
 	public void setColorGroup(String colorGroup) {
 		this.colorGroup = colorGroup;
 	}
 
+	/**
+	 * Sets the price of a house for this property.
+	 * @param housePrice The new house price to set.
+	 */
 	public void setHousePrice(int housePrice) {
 		this.housePrice = housePrice;
 	}
 
+	/**
+	 * Sets the number of houses currently built on this property.
+	 * @param numHouses The number of houses to set on the property.
+	 */
 	public void setNumHouses(int numHouses) {
 		this.numHouses = numHouses;
 	}
 
+	/**
+	 * Sets the selling price of this property.
+	 * @param sellPrice The price to set for selling the property.
+	 */
 	public void setPrice(int sellPrice) {
 		this.sellPrice = sellPrice;
 	}
 
+	/**
+	 * Sets the base rent amount for this property.
+	 * @param rent The rent amount to set for the property.
+	 */
 	public void setRent(int rent) {
 		this.rent = rent;
 	}
